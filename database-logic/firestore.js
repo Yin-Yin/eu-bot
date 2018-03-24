1e8f580985a7eca02b8eb9033e5681192c5816ae
Doktor Schwein <doktorschwein@gmail.com>
2018-03-24 09:39:32 +0000

    Add Firebase authentication

--- database-logic/firestore.js
+++ database-logic/firestore.js
@@ -2,7 +2,19 @@
 const admin = require('firebase-admin');
 
 //var serviceAccount = require('path/to/serviceAccountKey.json'); // set key to get from process.env. like process.env.serviceAccountKey
-var serviceAccount = require('path/to/serviceAccountKey.json');
+//var serviceAccount = require('path/to/serviceAccountKey.json');
+var serviceAccount = {
+    "type": "service_account",
+  "project_id": process.env.PROJECT_ID,
+  "private_key_id": process.env.PRIVATE_KEY_ID,
+  "private_key": process.env.PRIVATE_KEY,
+  "client_email": process.env.CLIENT_EMAIL,
+  "client_id": process.env.CLIENT_ID,
+  "auth_uri": process.env.AUTH_URI,
+  "token_uri":  process.env.TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
+}
 
 var db = admin.firestore();
 
