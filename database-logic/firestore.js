21283252763a0c8a49fe31f2565294aa280ed492
Doktor Schwein <doktorschwein@gmail.com>
2018-03-24 09:42:49 +0000

    Trying around with firestore

--- database-logic/firestore.js
+++ database-logic/firestore.js
@@ -5,18 +5,18 @@ const admin = require('firebase-admin');
 //var serviceAccount = require('path/to/serviceAccountKey.json');
 var serviceAccount = {
     "type": "service_account",
-  "project_id": process.env.PROJECT_ID,
-  "private_key_id": process.env.PRIVATE_KEY_ID,
-  "private_key": process.env.PRIVATE_KEY,
-  "client_email": process.env.CLIENT_EMAIL,
-  "client_id": process.env.CLIENT_ID,
-  "auth_uri": process.env.AUTH_URI,
-  "token_uri":  process.env.TOKEN_URI,
-  "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
-  "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
+    "project_id": process.env.PROJECT_ID,
+    "private_key_id": process.env.PRIVATE_KEY_ID,
+    "private_key": process.env.PRIVATE_KEY,
+    "client_email": process.env.CLIENT_EMAIL,
+    "client_id": process.env.CLIENT_ID,
+    "auth_uri": process.env.AUTH_URI,
+    "token_uri": process.env.TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
 }
 
-var db = admin.firestore();
+var db = {};
 
 module.exports = {
     initializeDatabase: function() {
@@ -24,18 +24,24 @@ module.exports = {
         admin.initializeApp({
             credential: admin.credential.cert(serviceAccount)
         });
+
+        this.db = admin.firestore();
         
+
+
     },
 
-    readFromFirestore: function(){
-        db.collection('abbreviations').get()
-    .then((snapshot) => {
-        snapshot.forEach((doc) => {
-            console.log(doc.id, '=>', doc.data());
-        });
-    })
-    .catch((err) => {
-        console.log('Error getting documents', err);
-    });
+    readFromFirestore: function() {
+                //this is to get sth out of the database 
+        this.db.collection('abbreviations').get()
+            .then((snapshot) => {
+                snapshot.forEach((doc) => {
+                    console.log(doc.id, '=>', doc.data());
+                });
+            })
+            .catch((err) => {
+                console.log('Error getting documents', err);
+            });
+        
     }
 }
