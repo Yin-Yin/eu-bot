4f6edfb854234eebc3e288e129c9cb4113f8beb6
Doktor Schwein <doktorschwein@gmail.com>
2019-01-13 10:01:41 +0000

    add auth

--- index.js
+++ index.js
@@ -5,6 +5,29 @@ var app = express();
 var dialogflowModule = require('./dialogflow/dialogflow.js');
 var firestoreModule = require('./database-logic/firestore.js');
 
+app.use((req, res, next) => {
+
+  // -----------------------------------------------------------------------
+  // authentication middleware
+
+  const auth = {login: process.env.login, password: process.env.password}
+
+  // parse login and password from headers
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+  const [login, password] = new Buffer(b64auth, 'base64').toString().split(':')
+
+  // Verify login and password are set and correct
+  if (!login || !password || login !== auth.login || password !== auth.password) {
+    res.set('WWW-Authenticate', 'Basic realm="401"') 
+    res.status(401).send('Authentication required.')
+    return
+  }
+
+  // -----------------------------------------------------------------------
+  // Access granted...
+  next()
+
+})
 
 console.log("starting server ..");
 firestoreModule.initializeDatabase();
