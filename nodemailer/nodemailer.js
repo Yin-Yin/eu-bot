7f37fcf98ce3aacb1870d71a53655d4ef85aeb63
Doktor Schwein <doktorschwein@gmail.com>
2019-01-19 20:14:25 +0000

    remove html in messages in nodemailer

--- nodemailer/nodemailer.js
+++ nodemailer/nodemailer.js
@@ -17,7 +17,7 @@ module.exports = {
             to: process.env.MAIL_FEEDBACKTO, // list of receivers
             subject: 'EU BOT Feedback', // Subject line
             text: "Hello there, \n this is the EU Bot. A user sent you the following text: '\n" + feedbackText + "\n' Have a nice day! \n Yours EU Bot.", // plain text body
-            html: '<h1>EU Bot Feedback Message</h1><p>Hello there,</p><p>this is the EU Bot. A user sent you the following text: </p><p>' + feedbackText // html body
+            // html: '<h1>EU Bot Feedback Message</h1><p>Hello there,</p><p>this is the EU Bot. A user sent you the following text: </p><p>' + feedbackText // html body
         };
 
         // send mail with defined transport object
